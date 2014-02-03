021b3e5dea00fad212640f6b85c655fceada9045
Mike <mike.oram@futurenet.com>
2014-02-03 14:53:11 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/core.css" type="text/css" rel="stylesheet" media="all">
+<!--[if lte IE 8]>
+	<link href="css/ie.css" type="text/css" rel="stylesheet" media="all">
+<![endif]-->
+<script type="text/javascript" src="js/jquery.1.9.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/page-nav.js"></script>
+
+<title>Mike Oram | Web Developer</title>
+</head>
+<body style="background: url('images/abstract-light.jpg') fixed center center no-repeat;" class="nojs">
+<div class="container">    
+    <nav id="home" class="navbar main">
+        <ul class="nav">
+            <li><a href="#about">
+                <img src="images/me.jpg">
+                <span>About Me</span>
+            </a></li>	
+            <li><a href="#portfolio">
+                <img src="images/portfolio.jpg">
+                <span>My Portfolio</span>
+            </a></li>
+            <li class="active logo"><a href="#home">
+            	<img src="images/title.jpg" alt="MikeOram - Web Developer">
+            	<h1>Mike Oram</h1>
+                <span>Home</span>
+            </a></li>
+            <li><a href="#skillset">
+                <img src="images/skills.jpg">
+                <span>My Skillset</span>
+            </a></li>
+            <li><a href="#contact">
+                <img src="images/contact.jpg">
+                <span>Contact Me</span>
+            </a></li>
+        </ul>
+    </nav>
+    
+    <div id="about" class="page">
+    	<div class="container">
+        	<a href="#home" class="close">&times;</a>
+            <div class="content">
+				<h2>About Me</h2>
+                <p>I have been working as a web developer for the past 6 years. I am skilled in design, front end and back end technologies and am always improving my skillset. I deliver unique, original, easy to use applications that clients and customers love.</p>
+
+<p>If you would like to see a sample of my work please take a look at my portfolio page where you will find a collection of my most recent and favourite jobs. I have worked for many clients ranging from local sole traders to large international companies. To list just a few of my clients, I have worked with: BikeRadar, Jaguar, Land Rover, BMW, MINI, Alfa Romeo, Cyclingnews, Staples, TNT, The Open University, Gatorade, Dubai Tourisum Centre, RSPCA, Bristol Outdoor Persuits, Strato Hosting, Bristol Favours and many more.</p>
+            </div>
+        </div>
+    </div>
+    
+    <div id="portfolio" class="page">
+    	<div class="container">
+        	<a href="#home" class="close">&times;</a>
+    		<div class="content">
+            	<h2>My Portfolio</h2>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/tnt.jpg" alt="TNT Post"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/jaguar.jpg" alt="Jaguar"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/grocer.jpg" alt="The Grocer"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/evoque.jpg" alt="Land Rover Evoque"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/fmh.jpg" alt="Find My Hospitality"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/bf.jpg" alt="Bristol Favours"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/bikely.jpg" alt="Bikely"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/flowers.jpg" alt="Flowers By Debs Design"></a>
+                <a href="#carousel" role="button" class="btn" data-toggle="modal"><img src="images/portfolio/flynn.jpg" alt="Flynn Product Design"></a>
+            </div>
+        </div>
+    </div>
+    
+    <div id="skillset" class="page">
+    	<div class="container">
+        	<a href="#home" class="close">&times;</a>
+    		<div class="content">
+            	<h2>My Skillset</h2>
+                <table width="100%" cellspacing="0" cellpadding="0" align="center">
+                    <tbody>
+                        <tr>
+                            <td>HTML</td>
+                            <td>5 Years+</td>
+                            <td class="level rating10" rel="1">10/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>CSS</td>
+                            <td>5 Years+</td>
+                            <td class="level rating10" rel="10">10/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>Javascript</td>
+                            <td>5 Years+</td>
+                            <td class="level rating6" rel="6">6/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>jQuery</td>
+                            <td>3 Years</td>
+                            <td class="level rating8" rel="8">8/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>PHP</td>
+                            <td>3 Years</td>
+                            <td class="level rating7" rel="7">7/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>CakePHP</td>
+                            <td>1 Year</td>
+                            <td class="level rating5" rel="5">5/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>SQL</td>
+                            <td>2 Years</td>
+                            <td class="level rating7" rel="7">7/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>SVN + GIT</td>
+                            <td>1 Year</td>
+                            <td class="level rating6" rel="6">6/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>XML</td>
+                            <td>2 Years</td>
+                            <td class="level rating8" rel="8">8/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>Photoshop</td>
+                            <td>4 Years</td>
+                            <td class="level rating7" rel="7">7/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>Flash</td>
+                            <td>2 Years</td>
+                            <td class="level rating7" rel="7">7/10<span></span></td>
+                        </tr>
+                        <tr>
+                            <td>Email Marketing</td>
+                            <td>3 Years</td>
+                            <td class="level rating9" rel="9">9/10<span></span></td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+            </div>
+        </div>
+    </div>
+    
+    <div id="contact" class="page">
+    	<div class="container">
+        	<a href="#home" class="close">&times;</a>
+    	<div class="content">
+            	<h2>Contact Me</h2>
+				<p>Please email me at <a href="mailto:emailme@mikeoram.co.uk">emailme@mikeoram.co.uk</a> for all enquiries and questions.<br>If you have a more urgent requirement you can ring me on <strong>07951&nbsp;634&nbsp;661</strong> 9am – 7pm 7 days a week.</p>
+
+				<p>To request a copy of my CV, please enter your email address below and you will be sent one straight away. You will be able to use my email forwarding feature to send the email to other addresses.</p>
+ 
+ 				<p>
+                <form action="http://eliteweb.createsend.com/t/r/s/auhddy/" method="post" id="subForm" class="left">
+                    <label for="auhddy-auhddy">Email:</label> <input name="cm-auhddy-auhddy" id="auhddy-auhddy" type="text">
+                    <input value="Request CV" type="submit">
+                </form>
+                </p>
+
+				<p>Please click here to download a <a href="http://www.mikeoram.co.uk/Curriculum_Vitae.pdf" target="_blank">conventional version of my CV</a>.</p>
+
+            </div>
+        </div>
+    </div>
+    
+    <div id="carousel" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-header">
+            <a class="close" data-dismiss="modal" aria-hidden="true">&times;</a>
+            <h3>My Portfolio</h3>
+        </div>
+        <div class="modal-body">
+        	<div id="myCarousel" class="carousel slide">
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+                    <div class="active item">
+                    	<img src="images/portfolio/tnt.jpg">
+                        <div class="carousel-caption">
+                          <h4>TNT Data Capture</h4>
+                          <p>I designed a built a data capture landing page to go with an email campaign promoting TNT Post's dataMatters service.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/jaguar.jpg">
+                        <div class="carousel-caption">
+                          <h4>Jaguar</h4>
+                          <p>Email campaign introducing the new Jaguar XF and promoting its 0% finance offer.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/grocer.jpg">
+                        <div class="carousel-caption">
+                          <h4>The Grocer</h4>
+                          <p>A subscriber preferences data collection page that feeds data from an email subscription form to create a personalised view for each user.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/evoque.jpg">
+                        <div class="carousel-caption">
+                          <h4>Range Rover Evoque</h4>
+                          <p>I headed up the design behind New Range Rover Evoque, this is one of the first banners ever released of the Evoque.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/fmh.jpg">
+                        <div class="carousel-caption">
+                          <h4>Find My Hospitality</h4>
+                          <p>From concept to deploy, I built this sports hospitality ticket comparison site with custom CMS, supplier user accounts and admin interface. <a href="http://www.findmyhospitality.com" target="_blank">Visit the website</a>.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/bf.jpg">
+                        <div class="carousel-caption">
+                          <h4>Bristol Favours</h4>
+                          <p>I designed and built an ecommerce Wedding Favours website, followed by building a email newsletter template. <a href="http://www.bristolfavours.co.uk" target="_blank">Visit the website</a>.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/bikely.jpg">
+                        <div class="carousel-caption">
+                          <h4>Bikely</h4>
+                          <p>Complete redesign and build of bikely.com using SASS and backbone.js. <a href="http://www.bikely.com" target="_blank">Visit the website</a>.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/flowers.jpg">
+                        <div class="carousel-caption">
+                          <h4>Flowers By Debs Design</h4>
+                          <p>A simple wordpress site with voucher form and newsletter sign up. <a href="http://www.flowersbydebsdesign.co.uk" target="_blank">Visit the website</a>.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                    	<img src="images/portfolio/flynn.jpg">
+                        <div class="carousel-caption">
+                          <h4>Flynn Product Design</h4>
+                          <p>A responsive, visually strong site with lots of fun animation. <a href="http://flynn-product-design.com/" target="_blank rel="nofollow">Visit the website</a>.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Carousel nav -->
+                <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+            </div>
+        </div>
+    </div>
+    
+    
+</div>
+</body>
+</html>
